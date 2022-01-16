873ca258e7d0ac7b761ca32228d7614c5f77a752
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-16 16:19:58 -0600

    added invoice line item dto

---
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Dtos/InvoiceLineItem.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoenixTheatreAPI.Dtos
+{
+    public class InvoiceLineItem
+    {
+        public int InvoiceId { get; set; }
+        public Tickets Tickets { get; set; }
+        public int Quantity { get; set; }
+    }
+}
