6d0d4040cd34f81320a663c4ac51a0aa354da2cd
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-16 16:20:39 -0600

    added order dto

---
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Dtos/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace PhoenixTheatreAPI.Dtos
+{
+    public class Order
+    {
+        public Guid OderId { get; set; }
+        public Guid CustomerId { get; set; }
+        public int TheatreId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public InvoiceLineItem InvoiceLineItem { get; set; }
+    }
+}
