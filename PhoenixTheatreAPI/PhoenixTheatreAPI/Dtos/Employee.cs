143e98f3673750b40f8a85722205b8bcd813ee76
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-16 15:36:34 -0600

    added employee dto

---
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Dtos/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace PhoenixTheatreAPI.Dtos
+{
+    public class Employee
+    {
+        Guid EmployeeId { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Username { get; set; }
+        string Password { get; set; }
+        string IsManager { get; set; }
+        int TheatreId { get; set; }
+    }
+}
