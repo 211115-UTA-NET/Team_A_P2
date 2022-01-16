ec83874b56fbd4a0590f555f1d0718a67a34ffd1
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-16 16:01:27 -0600

    added theatre dot

---
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Dtos/Theatre.cs
@@ -0,0 +1,10 @@
+﻿namespace PhoenixTheatreAPI.Dtos
+{
+    public class Theatre
+    {
+        public int TheatreId { get; set; }
+        public string TheatreLocation { get; set; }
+        public FilmShowings filmShowings { get; set; }
+        public Tickets tickets { get; set; }
+    }
+}
