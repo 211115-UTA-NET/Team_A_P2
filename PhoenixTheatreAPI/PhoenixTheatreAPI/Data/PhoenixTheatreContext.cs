5d29c55a728378e7f6383a38c4badfc1bf3eba8d
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-17 20:33:40 -0600

    Add the phoenix theatre context

---
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Data/PhoenixTheatreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoenixTheatreAPI.Dtos;
+
+namespace PhoenixTheatreAPI.Data
+{
+    public class PhoenixTheatreContext : DbContext
+    {
+        public PhoenixTheatreContext(DbContextOptions<PhoenixTheatreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customer => Set<Customer>();
+    }
+}
