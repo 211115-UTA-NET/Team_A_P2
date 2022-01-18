2834d8fd2cc36a9621ec35269fc36069cd1ad8a2
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-17 20:32:08 -0600

    Added the phoenixTheatreController

---
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Controllers/PhoenixTheatreController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoenixTheatreAPI.Dtos;
+using PhoenixTheatreAPI.Services;
+
+namespace PhoenixTheatreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PhoenixTheatreController : ControllerBase
+    {
+        PhoenixTheatreService _service;
+
+        public PhoenixTheatreController(PhoenixTheatreService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("customer")]
+        public IEnumerable<Customer> GetCustomerByName(string firstName, string lastName)
+        {
+            var customer = _service.GetCustomerByName(firstName, lastName);
+            return customer;
+        }
+
+    }
+}
