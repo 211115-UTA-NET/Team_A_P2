5235f774a84d4b4479fee38f61100acc24f2691c
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-17 20:20:51 -0600

    added the connection string to the api using user secrets

--- PhoenixTheatreAPI/PhoenixTheatreAPI/Program.cs
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Program.cs
@@ -1,4 +1,11 @@
+using PhoenixTheatreAPI.Data;
+using PhoenixTheatreAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("ConnectionStringPTDB");
+
+builder.Services.AddSqlServer<PhoenixTheatreContext>(connectionString);
 
 // Add services to the container.
 
@@ -6,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<PhoenixTheatreService>();
 
 var app = builder.Build();
 
