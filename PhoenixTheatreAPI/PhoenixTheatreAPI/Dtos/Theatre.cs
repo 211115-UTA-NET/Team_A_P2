287185bfed0c74fd4dc92928ebb3a7a1f6e31635
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-17 20:07:56 -0600

    updated theatre dto

--- PhoenixTheatreAPI/PhoenixTheatreAPI/Dtos/Theatre.cs
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Dtos/Theatre.cs
@@ -1,10 +1,10 @@
-﻿namespace PhoenixTheatreAPI.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoenixTheatreAPI.Dtos
 {
     public class Theatre
     {
         public int TheatreId { get; set; }
         public string TheatreLocation { get; set; }
-        public FilmShowings FilmShowings { get; set; }
-        public Tickets Tickets { get; set; }
     }
 }
