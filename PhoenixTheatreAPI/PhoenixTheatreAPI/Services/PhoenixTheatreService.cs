dfc3cb29a8503903234cdcd41bbe7a3edf5b7e4c
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-17 20:28:45 -0600

    added the phoenix theatre service

---
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Services/PhoenixTheatreService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoenixTheatreAPI.Data;
+using PhoenixTheatreAPI.Dtos;
+
+namespace PhoenixTheatreAPI.Services
+{
+    public class PhoenixTheatreService
+    {
+        private readonly PhoenixTheatreContext _context;
+        public  PhoenixTheatreService(PhoenixTheatreContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Customer> GetCustomerByName(string firstName, string lastName)
+        {
+            return _context.Customer;
+        }
+    }
+}
