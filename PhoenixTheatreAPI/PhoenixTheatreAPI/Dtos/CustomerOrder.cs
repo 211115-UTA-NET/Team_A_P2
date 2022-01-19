0453dd82b0c88aa52cce7b3701c039e8472af8c3
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-18 22:56:05 -0600

    added tests for customer and employee

--- PhoenixTheatreAPI/PhoenixTheatreAPI/Dtos/CustomerOrder.cs
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Dtos/CustomerOrder.cs
@@ -1,11 +1,13 @@
-﻿namespace PhoenixTheatreAPI.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoenixTheatreAPI.Dtos
 {
-    public class Order
+    public class CustomerOrder
     {
+        [Key]
         public Guid OrderId { get; set; }
         public Guid CustomerId { get; set; }
         public int TheatreId { get; set; }
         public decimal TotalPrice { get; set; }
-        public InvoiceLineItem InvoiceLineItem { get; set; }
     }
 }
