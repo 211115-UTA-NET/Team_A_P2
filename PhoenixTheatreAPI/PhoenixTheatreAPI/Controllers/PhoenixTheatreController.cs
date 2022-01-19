f00ffa4fd0cd7b0716dff28de25baa4fb5971754
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-18 22:51:18 -0600

    added methods to get an order, customer and employee

--- PhoenixTheatreAPI/PhoenixTheatreAPI/Controllers/PhoenixTheatreController.cs
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Controllers/PhoenixTheatreController.cs
@@ -15,12 +15,73 @@ namespace PhoenixTheatreAPI.Controllers
             _service = service;
         }
 
-        [HttpGet("customer")]
-        public IEnumerable<Customer> GetCustomerByName(string firstName, string lastName)
+        [HttpGet]
+        public FilmShowings GetAll()
         {
-            var customer = _service.GetCustomerByName(firstName, lastName);
-            return customer;
+            throw new NotImplementedException();
         }
 
+        [HttpGet("orderId")]
+        public ActionResult<CustomerOrder> GetOrderById(Guid orderId)
+        {
+            var order = _service.GetOrderById(orderId);
+
+            if(order != null)
+            {
+                return order;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("customer")]
+        public ActionResult<Customer> GetCustomerByName(string firstName, string lastName)
+        {
+            var customer = _service.GetCustomerByName(firstName, lastName);
+
+            if(customer != null)
+            {
+                return customer;
+            }
+            else
+            {
+                return NotFound();
+            }
+                
+        }
+
+        [HttpGet("employee")]
+        public ActionResult<Employee> GetEmployeeByName(string firstName, string lastName)
+        {
+            var employee = _service.GetEmployeeByName(firstName, lastName);
+            if(employee != null)
+            {
+                return employee;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost("{customer}")]
+        public IActionResult CreateCustomer(Customer newCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("{employee}")]
+        public IActionResult CreateEmployee(Employee newEmployee)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("{order}")]
+        public IActionResult CreateOrder(CustomerOrder newOrder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
