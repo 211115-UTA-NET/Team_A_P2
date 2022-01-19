0f84fe5b3046cfcf4f21a24579d3185e70529de7
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-18 22:55:14 -0600

    added methods to get an order, customer, and employee

--- PhoenixTheatreAPI/PhoenixTheatreAPI/Services/PhoenixTheatreService.cs
+++ PhoenixTheatreAPI/PhoenixTheatreAPI/Services/PhoenixTheatreService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using PhoenixTheatreAPI.Data;
+﻿using PhoenixTheatreAPI.Data;
 using PhoenixTheatreAPI.Dtos;
 
 namespace PhoenixTheatreAPI.Services
@@ -7,14 +6,72 @@ namespace PhoenixTheatreAPI.Services
     public class PhoenixTheatreService
     {
         private readonly PhoenixTheatreContext _context;
-        public  PhoenixTheatreService(PhoenixTheatreContext context)
+        public PhoenixTheatreService() { }
+        public PhoenixTheatreService(PhoenixTheatreContext context)
         {
             _context = context;
         }
 
-        public IEnumerable<Customer> GetCustomerByName(string firstName, string lastName)
+        public FilmShowings GetAll()
         {
-            return _context.Customer;
+            throw new NotImplementedException();
+        }
+
+        public CustomerOrder GetOrderById(Guid orderId)
+        {
+            var order = new CustomerOrder();
+            try
+            {
+                order = _context.Orders.Where(x => x.OrderId == orderId).Single();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return order;
+        }
+
+        public virtual Customer GetCustomerByName(string firstName, string lastName)
+        {
+            var customer = new Customer();
+            try
+            {
+                customer = _context.Customer.Where(x => x.FirstName == firstName && x.LastName == lastName).Single();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return customer;
+        }
+
+        public virtual Employee GetEmployeeByName(string firstName, string lastName)
+        {
+            var employee = new Employee();
+            try
+            {
+                employee = _context.Employee.Where(x => x.FirstName == firstName && x.LastName == lastName).Single();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return employee;
+        }
+
+        public Customer CreateCustomer(Customer newCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee CreateEmployee(Employee newEmployee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerOrder CreateOrder(CustomerOrder newOrder)
+        {
+            throw new NotImplementedException();
         }
     }
 }
