28221d55ff738b0ce5d3bdd62785c0cf5ebdc9e7
Edward cicio <edwardcicioc@gmail.com>
2022-01-26 12:06:40 -0500

    Add route for login screen and modify css for login's border

--- PhoenixTheatreAngular/src/app/app-routing.module.ts
+++ PhoenixTheatreAngular/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { MovieDescriptionComponent } from './movie-description/movie-description
 import { MoviesComponent } from './movies/movies.component';
 import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 import { MyOrderHistoryComponent } from './my-order-history/my-order-history.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: 'login', component: LoginScreenComponent},
   { path: 'movies', component: MoviesComponent, pathMatch: 'full' },
   { path: 'movie/:filmId', component: MovieDescriptionComponent },
   { path: 'shoppingcart', component: ShoppingcartComponent },
