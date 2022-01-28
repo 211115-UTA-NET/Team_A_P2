23ef952bb0b8a82283fdf6aa70322947c1572574
Travis <tboskowitz@gmail.com>
2022-01-27 20:09:15 -0600

    CORS package reference, I dunno anymore

--- PhoenixTheatreAngular/src/app/movie.service.ts
+++ PhoenixTheatreAngular/src/app/movie.service.ts
@@ -20,7 +20,7 @@ export class MovieService {
     const amounts = of(fakeTicketSelectionAmounts);
     return amounts;
   }
-  private url = "https://phoenixtheatreapi.azurewebsites.net/movies";
+  private url = "https://localhost:7291/PhoenixTheatre/movies";
 
   getMovies(): Observable<Movie[]> {
     return this.http.get<Movie[]>(this.url);
